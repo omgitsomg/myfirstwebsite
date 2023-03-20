5f05d30337b9e87c75def421fb0aee8f930972b9
Kevin Ly <lykt2@vcu.edu>
2023-03-20 19:34:56 -0400

    Made the simple grid to have multiple columns -> typo in column

--- components/Collapsible.js
+++ components/Collapsible.js
@@ -1,4 +1,4 @@
-import { Card, CardBody, CardHeader, Heading, SimpleGrid } from '@chakra-ui/react';
+import { Card, CardBody, CardHeader, Heading, SimpleGrid, Stack, Divider, CardFooter, ButtonGroup, Button, Image, Text } from '@chakra-ui/react';
 import styles from '../styles/Collapsible.module.css'
 
 const Collapsible = (props) => {
@@ -10,25 +10,23 @@ const Collapsible = (props) => {
     // weatherdescription
 
     return (
-        <div className={ styles.container }>
-            <Card size="sm" width="max-content">
-                <CardHeader>
-                    <Heading size="lg"> { props.date } </Heading>
-                </CardHeader>
-                <CardBody
-                    direction={{ base: 'column', sm: 'row' }}
-                    overflow="hidden"
-                >
-                    <i className={ `wi wi-owm-` + (props.weatherid).toString() + ` ${ styles.iconStyle }`}></i>
-                    <div>
-                        <p>Temperature: { props.temperature } </p>
-                        <p>Humidity: { props.humidity } </p>
-                        <p>Weather description: { props.weather_desc } </p>
-                        <Card></Card>
-                    </div>
-                </CardBody>
-            </Card>
-        </div>
+        <Card maxW="sm">
+            <CardHeader>
+                <Heading size="lg"> { props.date } </Heading>
+            </CardHeader>
+            <CardBody
+                direction={{ base: 'column', sm: 'row' }}
+                overflow="hidden"
+            >
+                <i className={ `wi wi-owm-` + (props.weatherid).toString() + ` ${ styles.iconStyle }`}></i>
+                <div>
+                    <p>Temperature: { props.temperature } </p>
+                    <p>Humidity: { props.humidity } </p>
+                    <p>Weather description: { props.weather_desc } </p>
+                    <Card></Card>
+                </div>
+            </CardBody>
+        </Card>
     );
 
     
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Homepage.module.css'
 import Collapsible from '../components/Collapsible'
-import { Input, SimpleGrid } from '@chakra-ui/react'
-import { Box } from '@chakra-ui/react'
+import { Input, SimpleGrid, Card, CardBody, CardHeader, Heading, Stack, Divider, CardFooter, ButtonGroup, Button, Image, Text } from '@chakra-ui/react';
 
 export default function Home() {
 
@@ -81,24 +78,13 @@ export default function Home() {
         </button>
         
       </div>
-      <SimpleGrid columns={3} spacing={10}>
-          <Box bg='tomato' height='80px'></Box>
-          <Box bg='tomato' height='80px'></Box>
-          <Box bg='tomato' height='80px'></Box>
-          <Box bg='tomato' height='80px'></Box>
-          <Box bg='tomato' height='80px'></Box>
+      <SimpleGrid columns={3} spacing={12} padding="2rem">
+        {
+          todaysweather.map(item => (
+            <Collapsible date={item[0]} temperature={item[1]} humidity={item[2]} weatherid={item[3]} weather_desc={item[4]} />
+          ))
+        }
       </SimpleGrid>
-      { 
-        // If the button has been clicked then show the div
-        buttonActive &&
-        <SimpleGrid column={4} spacing={10}>
-          {
-            todaysweather.map(item => (
-              <Collapsible date={item[0]} temperature={item[1]} humidity={item[2]} weatherid={item[3]} weather_desc={item[4]} />
-            ))
-          }
-        </SimpleGrid>
-      }
     </div>
   )
 }
