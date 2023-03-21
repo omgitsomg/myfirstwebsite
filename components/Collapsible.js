f450403d1f41f90fc1453390b30e5a7174e9d49d
Kevin Ly <lykt2@vcu.edu>
2023-03-21 01:06:43 -0400

    Added Color changes depending of the temperature

--- components/Collapsible.js
+++ components/Collapsible.js
@@ -9,11 +9,44 @@ const Collapsible = (props) => {
     // weatherid;
     // weatherdescription
 
-    var kelvinToFahrenheit = (props.temperature - 273.15) * (9/5) + 32;
-    kelvinToFahrenheit = kelvinToFahrenheit.toFixed(0);
+    // Convert Kelvin to fahrenheit
+    var fahrenheit = (props.temperature - 273.15) * (9/5) + 32;
+    fahrenheit = fahrenheit.toFixed(0);
+    var redOpacity = 0;
+    var blueOpacity = 0;
+    switch(true) {
+        case fahrenheit < 30:
+            redOpacity = 100;
+            blueOpacity = 400;
+            break;
+        case fahrenheit < 40:
+            redOpacity = 100;
+            blueOpacity = 300;
+            break;
+        case fahrenheit < 50:
+            redOpacity = 100;
+            blueOpacity = 200;
+            break;
+        case fahrenheit < 60:
+            redOpacity = 200;
+            blueOpacity = 200;
+            break;
+        case fahrenheit < 70:
+            redOpacity = 300;
+            blueOpacity = 100;
+            break;
+        case fahrenheit < 80:
+            redOpacity = 400;
+            blueOpacity = 100;
+            break;
+        default:
+            redOpacity = 500;
+            blueOpacity = 100;
+            break;
+    }
 
     return (
-        <Card>
+        <Card boxShadow="xl" bgGradient={`linear(to-br, red.${redOpacity}, blue.${blueOpacity})`}>
             <CardHeader>
                 <Heading size="lg"> { props.date } </Heading>
             </CardHeader>
@@ -23,7 +56,7 @@ const Collapsible = (props) => {
                     <i className={ `wi wi-owm-` + (props.weatherid).toString() + ` ${ styles.iconStyle }`}></i>
                     <VStack align="flex-start"
                         divider={<StackDivider borderColor='gray.500' />}>
-                        <p>Temperature: { kelvinToFahrenheit } </p>
+                        <p>Temperature: { fahrenheit } </p>
                         <p>Humidity: { props.humidity } </p>
                         <p>Weather description: { props.weather_desc } </p>
                         <Card></Card>
