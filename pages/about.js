c70d6d8ec6b32352bf00d1912f978f3ba5a9dd65
Kevin Ly <lykt2@vcu.edu>
2023-05-11 01:51:32 -0400

    About Page - Different Dimension formatting fix with Media Queries

--- pages/about.js
+++ pages/about.js
@@ -6,15 +6,19 @@ const about = () => {
     return (
         <div className={ styles.container }>
             <div className={ styles.bio }>
-                <Image src="/fullportrait.jpg" height="55vh" alt="Picture of Kevin Ly"></Image>
+                <Image 
+                    src="/fullportrait.jpg" 
+                    alt="Picture of Kevin Ly"
+                    className={ styles.imageStyle }
+                />
                 <div className={ styles.aboutMeTextWrapper }>
-                    <Heading>Hey everyone, I&apos;m Kevin Ly.</Heading>
-                    <Text fontSize="3xl" width="20em" whiteSpace="normal" mb="6">I am a senior at VCU majoring in computer science 
+                    <h2 className={ styles.bioTitle }>Hey everyone, I&apos;m Kevin Ly.</h2>
+                    <p className={ styles.bioText }>I am a senior at VCU majoring in computer science 
                     with a concentration in software engineering and a minor in mathematics. This weather forecast website I made was designed to display the weather
-                    for the next five days with three-hour intervals. My goals and motivations for this project were to practice and understand NextJS, Chakra UI, and JavaScript.</Text>
-                    <Text fontSize="2xl">Links:</Text>
-                    <Text fontSize="xl" textDecoration="underline"><a href="https://www.linkedin.com/in/kevin-ly-7446941a2/">LinkedIn</a></Text>
-                    <Text fontSize="xl" textDecoration="underline"><a href="https://github.com/omgitsomg">Github</a></Text>
+                    for the next five days with three-hour intervals. My goals and motivations for this project were to practice and understand NextJS, Chakra UI, and JavaScript.</p>
+                    <Text>Links:</Text>
+                    <Text textDecoration="underline"><a href="https://www.linkedin.com/in/kevin-ly-7446941a2/">LinkedIn</a></Text>
+                    <Text textDecoration="underline"><a href="https://github.com/omgitsomg">Github</a></Text>
                 </div>
             </div>
         </div>
